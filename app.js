1bedda8459f7f44c5daa7e611ec8bc12fc5261db
Kejia <daikejiajp@yahoo.co.jp>
2020-06-17 22:01:41 +0900

    score calculate

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const width = 10;
   let nextRandom = 0;
   let timerId;
+  let score = 0;
 
   //The Tetrominoes
   const lTetromino = [
@@ -112,6 +113,7 @@ document.addEventListener("DOMContentLoaded", () => {
       currentPosition = 4;
       draw();
       displayShape();
+      addScore();
     }
   }
 
@@ -200,6 +202,36 @@ document.addEventListener("DOMContentLoaded", () => {
       displayShape();
     }
   });
+
+  function addScore() {
+    for (let i = 0; i < 199; i += width) {
+      const row = [
+        i,
+        i + 1,
+        i + 2,
+        i + 3,
+        i + 4,
+        i + 5,
+        i + 6,
+        i + 7,
+        i + 8,
+        i + 9,
+      ];
+
+      if (row.every((index) => squares[index].classList.contains("taken"))) {
+        score += 10;
+        scoreDisplay.innerHTML = score;
+        row.forEach((index) => {
+          squares[index].classList.remove("taken");
+        });
+        const squaresRemoved = squares.splice(i, width);
+        // for (let j = i; j < width + i; j++) {
+        //   squaresRemoved.push(squares[j]);
+        // }
+        console.log(score, squaresRemoved);
+      }
+    }
+  }
   // console.log("test");
   // draw();
 });
